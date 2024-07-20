58cbedd78bc18cdadde84d6e416fcae4ad273981
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-07-20 12:21:42 -0400

    Fix lint errors

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,12 +2,12 @@
 from .restapis import get_request, analyze_review_sentiments, post_review
 
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
+# from django.contrib import messages
+# from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -66,7 +66,7 @@ def logout_request(request):
 
 @csrf_exempt
 def registration(request):
-    context = {}
+    # context = {}
 
     data = json.loads(request.body)
     username = data['userName']
@@ -75,7 +75,7 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exist = False
+    # email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
@@ -110,7 +110,6 @@ def get_dealerships(request, state="All"):
     else:
         endpoint = "/fetchDealers/" + state
 
-    print(" Endpoint: ", endpo)
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
 
@@ -148,11 +147,11 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status": 200})
+            return JsonResponse({"status": 200, "res": response})
         except BaseException:
             return JsonResponse(
                 {"status": 401, "message": "Error in posting review"})
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,9 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    "https://madechanguta-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+    "https://madechanguta-8000." +
+    "theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 CSRF_TRUSTED_ORIGINS = [
-    "https://madechanguta-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+    "https://madechanguta-8000." +
+    "theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -95,7 +97,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.' +
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
